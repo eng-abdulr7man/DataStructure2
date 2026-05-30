4343adfd4957b684401b200674f77e8855f2a350
AbdulRahman AbdulJalil <117041593+eng-abdulr7man@users.noreply.github.com>
2026-05-30 20:45:30 +0300

    Add files via upload

---
+++ Boxing-UnBoxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Boxing_UnBoxing
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//boxing 
+			int val = 10;
+			object obj = val;
+
+			Console.WriteLine("val: " + val);
+			Console.WriteLine("obj(boxing): " + obj);
+
+			//unboxing
+			int val2 = (int)obj;
+			Console.WriteLine("val2(unboxing): " + val2);
+		}
+	}
+}
